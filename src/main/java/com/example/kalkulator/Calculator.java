8915e6c407d7f9947e7b24f58005631247aa7acb
Filip Sokolowski <filipsokolowskiah@gmail.com>
2022-05-04 10:37:14 +0200

    Kalkulator

--- src/main/java/com/example/kalkulator/Calculator.java
+++ src/main/java/com/example/kalkulator/Calculator.java
@@ -1,2 +1,23 @@
-package com.example.kalkulator;public class Calculator {
+package com.example.kalkulator;
+
+import java.sql.SQLOutput;
+
+public class Calculator {
+
+    public int addingNumbers(int a, int b){
+            return a + b;
+    }
+
+    public int substractionNumbers(int a, int b){
+            return a - b;
+    }
+    public static void main(String args[]) {
+        Calculator calculator = new Calculator();
+        int addAToB = calculator.addingNumbers(4,5);
+        int substractAToB = calculator.substractionNumbers(9, 5);
+
+        System.out.println(addAToB);
+        System.out.println(substractAToB);
+
+    }
 }
